64e235c66921191fbfca9a38a31a236eb804a18c
Ramiro-isla <ramiroisla11@gmail.com>
2022-12-22 19:16:20 +0100

    tests done

---
+++ src/scripts/FizzBuzz.js
@@ -0,0 +1,12 @@
+export function fizzBuzz(numb) {
+    if (numb%3==0 && numb%5!==0) {
+        return "Fizz"
+    }
+    if (numb%5==0 && numb%3!==0) {
+        return "Buzz"
+    }
+    if (numb%3==0  && numb%3==0) {
+        return "FizzBuzz"
+    }
+    
+}
--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -1,5 +1,21 @@
-describe('FizzBuzz', () => {
-    test('should return Fizz', () => {
-        
+import { fizzBuzz } from "../src/scripts/FizzBuzz";
+
+describe("FizzBuzz", () => {
+    test("should return Fizz if divisible by three", () => {
+      let numb = 3;
+      const result = fizzBuzz(numb);
+      expect(result).toBe("Fizz");
     });
-});
+  
+    test("should return Buzz if divisible by five", () => {
+      let numb = 5;
+      const result = fizzBuzz(numb);
+      expect(result).toBe("Buzz");
+    });
+  
+    test("should return FizzBuzz if divisible by five and three", () => {
+        let numb = 15;
+      const result = fizzBuzz(numb);
+      expect(result).toBe("FizzBuzz");
+    });
+  });
