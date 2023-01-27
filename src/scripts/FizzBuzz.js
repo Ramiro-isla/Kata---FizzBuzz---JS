b8c34814c62f9f4dc9d1870b53a977613f1b0c68
Ramiro-isla <ramiroisla11@gmail.com>
2023-01-27 20:19:23 +0100

    test and exercice done

--- src/scripts/FizzBuzz.js
+++ src/scripts/FizzBuzz.js
@@ -1,12 +1,12 @@
 export function fizzBuzz(numb) {
-    if (numb%3==0 && numb%5!==0) {
-        return "Fizz"
-    }
-    if (numb%5==0 && numb%3!==0) {
-        return "Buzz"
-    }
-    if (numb%3==0  && numb%3==0) {
-        return "FizzBuzz"
-    }
-    
+  if (numb % 3 == 0 && numb % 5 !== 0) {
+    return "Fizz";
+  }
+  if (numb % 5 == 0 && numb % 3 !== 0) {
+    return "Buzz";
+  }
+  if (numb % 3 == 0 && numb % 5 == 0) {
+    return "FizzBuzz";
+  }
+  return numb;
 }
--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -1,21 +1,27 @@
 import { fizzBuzz } from "../src/scripts/FizzBuzz";
 
 describe("FizzBuzz", () => {
-    test("should return Fizz if divisible by three", () => {
-      let numb = 3;
-      const result = fizzBuzz(numb);
-      expect(result).toBe("Fizz");
-    });
-  
-    test("should return Buzz if divisible by five", () => {
-      let numb = 5;
-      const result = fizzBuzz(numb);
-      expect(result).toBe("Buzz");
-    });
-  
-    test("should return FizzBuzz if divisible by five and three", () => {
-        let numb = 15;
-      const result = fizzBuzz(numb);
-      expect(result).toBe("FizzBuzz");
-    });
+  test("should return Fizz if divisible by three", () => {
+    let numb = 3;
+    const result = fizzBuzz(numb);
+    expect(result).toBe("Fizz");
   });
+
+  test("should return Buzz if divisible by five", () => {
+    let numb = 5;
+    const result = fizzBuzz(numb);
+    expect(result).toBe("Buzz");
+  });
+
+  test("should return FizzBuzz if divisible by five and three", () => {
+    let numb = 15;
+    const result = fizzBuzz(numb);
+    expect(result).toBe("FizzBuzz");
+  });
+});
+test("should return FizzBuzz if divisible by five and three", () => {
+  for (let index = 0; index <= 100; index++) {
+    const result = fizzBuzz(index);
+    console.log(result);
+  }
+});
